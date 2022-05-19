855499718b448f5de16414f67dafffb9487ad226
SandrLite <mrlad@mail.ru>
2022-05-19 10:17:15 +0300

    Создал меню

--- HomeWork_3/Program.cs
+++ HomeWork_3/Program.cs
@@ -10,6 +10,53 @@ namespace HomeWork_3
     {
         static void Main(string[] args)
         {
+            StartMenu();
+        }
+
+        /// <summary>
+        /// Вывод меню в консоль.
+        /// </summary>
+        static void IntroMenu()
+        {
+            Console.WriteLine("\nВЫБЕРЕТЕ НУЖНЫЙ ПУНКТ ИЗ МЕНЮ НИЖЕ:");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++");            
+            Console.WriteLine("1 - Двухмерный массив по диагонали.");
+            Console.WriteLine("2 - Телефонный справочник.");
+            Console.WriteLine("3 - Строка в обратном порядке.");
+            Console.WriteLine("0 - Чтобы выйти из программы.");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("Укажите номер:");
+        }
+        /// <summary>
+        /// Вывод меню в консоль.
+        /// </summary>
+        static void StartMenu()
+        {
+            while (true)
+            {
+                IntroMenu();
+                string rezult = Console.ReadLine();
+
+                switch (rezult)
+                {
+                    case "1":
+                        Console.WriteLine("Выбрано 1");                        
+                        break;
+                    case "2":
+                        Console.WriteLine("Выбрано 2");
+                        break;
+                    case "3":
+                        Console.WriteLine("Выбрано 3");
+                        break;
+                    case "0":
+                        Console.WriteLine("Завершение программы.");
+                        Console.ReadKey();
+                        return;                        
+                    default:
+                        Console.WriteLine("Не определено. Пожалуйста, укажите цифру повторно.");
+                        break;
+                }
+            }            
         }
     }
 }
