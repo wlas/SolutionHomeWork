1bcce6dbd9bab030c5d7740b04433738d1b259e0
SandrLite <mr.wlas@mail.ru>
2022-05-25 17:20:39 +0300

    Создал меню

--- HomeWork_4/Program.cs
+++ HomeWork_4/Program.cs
@@ -10,6 +10,53 @@ namespace HomeWork_4
     {
         static void Main(string[] args)
         {
+            StartMenu();
+        }
+
+        /// <summary>
+        /// Вывод меню в консоль.
+        /// </summary>
+        static void IntroMenu()
+        {
+            Console.WriteLine("\nВЫБЕРЕТЕ НУЖНЫЙ ПУНКТ ИЗ МЕНЮ НИЖЕ:");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("1 - Задание 1");
+            Console.WriteLine("2 - Задание 2");
+            Console.WriteLine("3 - Задание 3");
+            Console.WriteLine("0 - Чтобы выйти из программы.");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++");
+            Console.Write("Укажите номер: ");
+        }
+        /// <summary>
+        /// Вывод меню в консоль.
+        /// </summary>
+        static void StartMenu()
+        {
+            while (true)
+            {
+                IntroMenu();
+                string rezult = Console.ReadLine();
+
+                switch (rezult)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    case "0":
+                        Console.WriteLine("Завершение программы.");
+                        Console.ReadKey();
+                        return;
+                    default:
+                        Console.WriteLine("Не определено. Пожалуйста, укажите цифру повторно.");
+                        break;
+                }
+            }
         }
     }
 }
