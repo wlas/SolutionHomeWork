62ce4810aeb564e8e0b9fc97ba7cd99eafae6573
SandrLite <mr.wlas@mail.ru>
2022-06-08 17:35:39 +0300

    Создали прорисовку окон

---
+++ ConsoleFileManager/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleFileManager
+{
+    internal class Program
+    {
+        const int WINDOW_HEIGHT = 30;
+        const int WINDOW_WIDTH = 120;
+        private static string currentDir = Directory.GetCurrentDirectory();
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Title = "FileManager";
+
+            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+            Console.SetBufferSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+            DrawWindow(0, 0, WINDOW_WIDTH, 18);
+            DrawWindow(0, 18, WINDOW_WIDTH, 8);
+            UpdateConsole();
+
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Обработка процесса ввода данных с консоли
+        /// </summary>
+        /// <param name="width">Длина строки ввода</param>
+        static void ProcessEnterCommand(int width)
+        {
+            //TODO: ...
+        }
+
+        /// <summary>
+        /// Обновление ввода с консоли
+        /// </summary>
+        static void UpdateConsole()
+        {
+            DrawConsole(currentDir, 0, 26, WINDOW_WIDTH, 3);
+            ProcessEnterCommand(WINDOW_WIDTH);
+        }
+
+
+        /// <summary>
+        /// Отрисовка консоли
+        /// </summary>
+        /// <param name="dir"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        static void DrawConsole(string dir, int x, int y, int width, int height)
+        {
+            DrawWindow(x, y, width, height);
+            Console.SetCursorPosition(x + 1, y + height / 2);
+            Console.Write($"{dir}>");
+        }
+
+        /// <summary>
+        /// Отрисовка окна
+        /// </summary>
+        /// <param name="x">Начальная позиция по оси X</param>
+        /// <param name="y">Начальная позиция по оси Y</param>
+        /// <param name="width">Ширина окна</param>
+        /// <param name="height">Высота окна</param>
+        static void DrawWindow(int x, int y, int width, int height)
+        {
+            // header - шапка
+            Console.SetCursorPosition(x, y);
+            Console.Write("╔");
+            for (int i = 0; i < width - 2; i++)
+                Console.Write("═");
+            Console.Write("╗");
+
+
+            // window - окно
+            Console.SetCursorPosition(x, y + 1);
+            for (int i = 0; i < height - 2; i++)
+            {
+                Console.Write("║");
+
+                for (int j = x + 1; j < x + width - 1; j++)
+                    Console.Write(" ");
+
+                Console.Write("║");
+            }
+
+            // footer - подвал
+            Console.Write("╚");
+            for (int i = 0; i < width - 2; i++)
+                Console.Write("═");
+            Console.Write("╝");
+            Console.SetCursorPosition(x, y);
+
+        }
+    }
+}
