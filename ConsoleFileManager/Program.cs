a179fc5cbeb9361bf53b83eb283f5d982730133d
SandrLite <mr.wlas@mail.ru>
2022-06-13 16:23:33 +0300

    Добавил функционал

--- ConsoleFileManager/Program.cs
+++ ConsoleFileManager/Program.cs
@@ -1,10 +1,7 @@
 ﻿using FileManager.Utils;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleFileManager
 {
@@ -15,17 +12,24 @@ namespace ConsoleFileManager
         private static string currentDir = Directory.GetCurrentDirectory();
         static void Main(string[] args)
         {
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.Title = "FileManager";
+            try
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Title = "FileManager";
 
-            Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-            Console.SetBufferSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+                Console.SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+                Console.SetBufferSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-            DrawWindow(0, 0, WINDOW_WIDTH, 18);
-            DrawWindow(0, 18, WINDOW_WIDTH, 8);
-            UpdateConsole();
+                DrawWindow(0, 0, WINDOW_WIDTH, 18);
+                DrawWindow(0, 18, WINDOW_WIDTH, 8);
+                UpdateConsole();
 
-            Console.ReadLine();
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         /// <summary>
@@ -83,12 +87,48 @@ namespace ConsoleFileManager
                         Console.SetCursorPosition(left, top);
                     }
                 }
-
             }
             while (keyInfo.Key != ConsoleKey.Enter);
             ParseCommandString(command.ToString());
         }
-
+        /// <summary>
+        /// Выводит на консоль информацию о файле
+        /// </summary>
+        /// <param name="file">Ссылка на файл</param>
+        static void FInfo(FileInfo file)
+        {
+            Console.SetCursorPosition(2, 19);
+            Console.WriteLine(file.Attributes);
+            Console.SetCursorPosition(2, 20);
+            Console.WriteLine(file.CreationTime);
+            Console.SetCursorPosition(2, 21);
+            Console.WriteLine(file.LastAccessTime);
+            Console.SetCursorPosition(2, 22);
+            Console.WriteLine(file.Extension);
+            Console.SetCursorPosition(2, 23);
+            Console.WriteLine(file.Length + " bytes actual size");
+        }
+        /// <summary>
+        /// Вывод на консоль информации о папке
+        /// </summary>
+        /// <param name="dir">Путь к папке</param>
+        static void DInfo(DirectoryInfo dir)
+        {
+            Console.SetCursorPosition(2, 19);
+            Console.WriteLine(dir.Attributes);
+            Console.SetCursorPosition(2, 20);
+            Console.WriteLine(dir.CreationTime);
+            Console.SetCursorPosition(2, 21);
+            Console.WriteLine(dir.LastAccessTime);
+            Console.SetCursorPosition(2, 22);
+            Console.WriteLine("Exists: " + dir.Exists);
+            Console.SetCursorPosition(2, 23);
+            Console.WriteLine(dir.FullName);
+        }
+        /// <summary>
+        /// Метод выполняет команды введенные пользователем
+        /// </summary>
+        /// <param name="command">Команда переданная пользователем</param>
         static void ParseCommandString(string command)
         {
             string[] commandParams = command.ToLower().Split(' ');
@@ -101,22 +141,233 @@ namespace ConsoleFileManager
                             if (Directory.Exists(commandParams[1]))
                             {
                                 currentDir = commandParams[1];
+                                Properties.Settings.Default.Path = currentDir;
+                                Properties.Settings.Default.Save();
                             }
                         break;
                     case "ls":
-                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
-                            if (commandParams.Length > 3 && commandParams[2] == "-p" && int.TryParse(commandParams[3], out int n))
+                        if (commandParams.Length > 1)
+                        {
+                            if (commandParams.Length > 2 && commandParams[1] == "-p" && int.TryParse(commandParams[2], out int n))
                             {
-                                DrawTree(new DirectoryInfo(commandParams[1]), n);
+                                DrawTree(new DirectoryInfo(currentDir), n, false);
                             }
-                            else
+                            else if (commandParams[1] == "-s")
                             {
-                                DrawTree(new DirectoryInfo(commandParams[1]), 1);
+                                DrawTree(new DirectoryInfo(currentDir), 1, true);
                             }
+                            else if (commandParams[1] == "-sp" && int.TryParse(commandParams[2], out int s))
+                            {
+                                DrawTree(new DirectoryInfo(currentDir), s, true);
+                            }
+                        }
+                        else
+                        {
+                            DrawTree(new DirectoryInfo(currentDir), 1, false);
+                        }
                         break;
+                    case "cp":
+                        if (commandParams.Length > 2)
+                        {
+                            if (File.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                if (Directory.Exists(commandParams[2]))
+                                {
+
+
+                                    File.Create(commandParams[2] + @"\" + commandParams[1]).Dispose();
+                                    File.Copy(currentDir + @"\" + commandParams[1], (commandParams[2] + @"\" + commandParams[1]), true);
+
+
+                                }
+                                else if (!Directory.Exists(currentDir + @"\" + commandParams[2]))
+                                {
+                                    Directory.CreateDirectory(commandParams[2]);
+                                    File.Create(commandParams[2] + @"\" + commandParams[1]).Dispose();
+                                    File.Copy(commandParams[1], (commandParams[2] + @"\" + commandParams[1]), true);
+
+                                }
+                            }
+                            else if (Directory.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                if (!Directory.Exists(commandParams[2]))
+                                {
+                                    Directory.CreateDirectory(commandParams[2]);
+                                    CopyDirectory(currentDir + @"\" + commandParams[1], commandParams[2], true);
+
+                                }
+                                else
+                                {
+                                    CopyDirectory(currentDir + @"\" + commandParams[1], commandParams[2], true);
+                                }
+                            }
+                        }
+                        break;
+                    case "info":
+                        if (commandParams.Length > 1)
+                        {
+                            if (File.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                DrawWindow(0, 18, WINDOW_WIDTH, 8);
+                                System.Threading.Thread.Sleep(100);
+                                FInfo(new FileInfo(currentDir + @"\" + commandParams[1]));
+                            }
+                            else if (Directory.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                DrawWindow(0, 18, WINDOW_WIDTH, 8);
+                                System.Threading.Thread.Sleep(100);
+                                DInfo(new DirectoryInfo(currentDir + @"\" + commandParams[1]));
+                            }
+                        }
+                        break;
+                    case "rm":
+                        if (commandParams.Length > 1)
+                        {
+                            if (File.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                File.Delete(currentDir + @"\" + commandParams[1]);
+                            }
+                            else if (Directory.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                DelDirectory(currentDir + @"\" + commandParams[1]);
+                            }
+                        }
+                        break;
+                    case "mv":
+                        if (commandParams.Length > 2)
+                        {
+                            if (File.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                if (Directory.Exists(commandParams[2]))
+                                {
+                                    File.Create(commandParams[2] + @"\" + commandParams[1]).Dispose();
+                                    File.Copy(currentDir + @"\" + commandParams[1], (commandParams[2] + @"\" + commandParams[1]), true);
+                                    File.Delete(currentDir + @"\" + commandParams[1]);
+                                }
+                                else if (!Directory.Exists(currentDir + @"\" + commandParams[2]))
+                                {
+                                    Directory.CreateDirectory(commandParams[2]);
+                                    File.Create(commandParams[2] + @"\" + commandParams[1]).Dispose();
+                                    File.Copy(commandParams[1], (commandParams[2] + @"\" + commandParams[1]), true);
+                                    File.Delete(commandParams[1]);
+                                }
+                            }
+                            else if (Directory.Exists(currentDir + @"\" + commandParams[1]))
+                            {
+                                if (!Directory.Exists(commandParams[2]))
+                                {
+                                    Directory.CreateDirectory(commandParams[2]);
+                                    CopyDirectory(currentDir + @"\" + commandParams[1], commandParams[2], true);
+                                    DelDirectory(currentDir + @"\" + commandParams[1]);
+                                }
+                                else
+                                {
+                                    CopyDirectory(currentDir + @"\" + commandParams[1], commandParams[2], true);
+                                    DelDirectory(currentDir + @"\" + commandParams[1]);
+                                }
+                            }
+                        }
+                        break;
+                    case "mkdir":
+                        if (commandParams.Length > 1)
+                        {
+
+                            if (!Directory.Exists(commandParams[1]))
+                            {
+                                Directory.CreateDirectory(currentDir + @"\" + commandParams[1]);
+                            }
+                        }
+                        break;
+                    case "touch":
+                        if (commandParams.Length != 0)
+                        {
+                            if (!File.Exists(commandParams[1]))
+                            {
+                                File.Create(currentDir + @"\" + commandParams[1]).Dispose();
+                            }
+                        }
+                        break;
+                    case "save":
+                        Properties.Settings.Default.Save();
+                        break;
+                    case "load":
+                        currentDir = Properties.Settings.Default.Path;
+                        break;
+                    case "exit":
+                        Properties.Settings.Default.Save();
+                        System.Environment.Exit(0);
+                        break;
+                    case "page-items":
+                        if (commandParams.Length > 1)
+                        {
+                            if (uint.TryParse(commandParams[1], out uint pageElements))
+                            {
+                                Properties.Settings.Default.Element = pageElements;
+                                Properties.Settings.Default.Save();
+                            }
+                        }
+                        break;
+
                 }
             }
             UpdateConsole();
+        }
+
+        // Копирование каталога и подкаталогов
+        static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+        {
+            var dir = new DirectoryInfo(sourceDir);
+
+            if (!dir.Exists)
+            {
+                return;
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            Directory.CreateDirectory(destinationDir);
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(destinationDir, file.Name);
+                file.CopyTo(targetFilePath);
+            }
+
+
+            if (recursive)
+            {
+                foreach (DirectoryInfo subDir in dirs)
+                {
+                    string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                    CopyDirectory(subDir.FullName, newDestinationDir, true);
+                }
+            }
+        }
+        // Рекурсивное удаление каталога (моё)
+        static void DelDirectory(string sourceDir)
+        {
+            var dir = new DirectoryInfo(sourceDir);
+
+            if (!dir.Exists)
+            {
+                return;
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                string targetFilePath = Path.Combine(sourceDir, file.Name);
+                file.Delete();
+            }
+
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                DelDirectory(subDir.FullName);
+            }
+
+            Directory.Delete(sourceDir, true);
+
         }
 
         static string GetShortPath(string path)
@@ -126,8 +377,6 @@ namespace ConsoleFileManager
             return shortPathName.ToString();
         }
 
-
-
         /// <summary>
         /// Обновление ввода с консоли
         /// </summary>
@@ -136,7 +385,6 @@ namespace ConsoleFileManager
             DrawConsole(GetShortPath(currentDir), 0, 26, WINDOW_WIDTH, 3);
             ProcessEnterCommand(WINDOW_WIDTH);
         }
-
 
         /// <summary>
         /// Отрисовка консоли
@@ -169,7 +417,6 @@ namespace ConsoleFileManager
                 Console.Write("═");
             Console.Write("╗");
 
-
             // window - окно
             Console.SetCursorPosition(x, y + 1);
             for (int i = 0; i < height - 2; i++)
@@ -196,10 +443,18 @@ namespace ConsoleFileManager
         /// </summary>
         /// <param name="dir">Директория</param>
         /// <param name="page">Страница</param>
-        static void DrawTree(DirectoryInfo dir, int page)
+        /// <param name="useSettingsFile"></param>
+        static void DrawTree(DirectoryInfo dir, int page, bool useSettingsFile)
         {
             StringBuilder tree = new StringBuilder();
-            GetTree(tree, dir, "", true);
+            if (useSettingsFile)
+            {
+                GetTree(tree, dir, "", true, true);
+            }
+            else
+            {
+                GetTree(tree, dir, "", true, false);
+            }
             DrawWindow(0, 0, WINDOW_WIDTH, 18);
             (int currentLeft, int currentTop) = GetCursorPosition();
             int pageLines = 16;
@@ -224,40 +479,82 @@ namespace ConsoleFileManager
 
         }
 
-        static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory)
+        static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory, bool useSettingsFile)
         {
-            tree.Append(indent);
-            if (lastDirectory)
+            try
             {
-                tree.Append("└─");
-                indent += "  ";
-            }
-            else
-            {
-                tree.Append("├─");
-                indent += "│ ";
-            }
-
-            tree.Append($"{dir.Name}\n");
-
-
-            FileInfo[] subFiles = dir.GetFiles();
-            for (int i = 0; i < subFiles.Length; i++)
-            {
-                if (i == subFiles.Length - 1)
+                tree.Append(indent);
+                if (lastDirectory)
                 {
-                    tree.Append($"{indent}└─{subFiles[i].Name}\n");
+                    tree.Append("└─");
+                    indent += "  ";
                 }
                 else
                 {
-                    tree.Append($"{indent}├─{subFiles[i].Name}\n");
+                    tree.Append("├─");
+                    indent += "│ ";
+                }
+
+                tree.Append($"{dir.Name}\n");
+
+
+                FileInfo[] subFiles = dir.GetFiles();
+                if (useSettingsFile)
+                    Array.Resize(ref subFiles, (int)Properties.Settings.Default.Element);
+                if (!useSettingsFile)
+                {
+                    for (int i = 0; i < subFiles.Length; i++)
+                    {
+                        if (i == subFiles.Length - 1)
+                        {
+                            tree.Append($"{indent}└─{subFiles[i].Name}\n");
+                        }
+                        else
+                        {
+                            tree.Append($"{indent}├─{subFiles[i].Name}\n");
+                        }
+                    }
+                }
+                else
+                {
+                    int i = 0;
+
+                    do
+                    {
+                        if (i == subFiles.Length - 1)
+                        {
+                            if (subFiles[i] != null)
+                                tree.Append($"{indent}└─{subFiles[i].Name}\n");
+                        }
+                        else
+                        {
+                            if (subFiles[i] != null)
+                                tree.Append($"{indent}├─{subFiles[i].Name}\n");
+
+                        }
+                        i++;
+                    } while (subFiles != null && i < subFiles.Length);
+                }
+
+                DirectoryInfo[] subDirects = dir.GetDirectories();
+
+                for (int i = 0; i < subDirects.Length; i++)
+                {
+                    try
+                    {
+                        GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1, useSettingsFile);
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1, useSettingsFile);
+                    }
+                    catch (Exception)
+                    {
+                        Console.Write("XX");
+                    }
                 }
             }
-
-
-            DirectoryInfo[] subDirects = dir.GetDirectories();
-            for (int i = 0; i < subDirects.Length; i++)
-                GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1);
+            catch { }
         }
     }
 }
