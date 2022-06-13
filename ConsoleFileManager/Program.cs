c86f80aba824691f6d6ebd8e5389d2a602835bec
SandrLite <mr.wlas@mail.ru>
2022-06-13 14:17:16 +0300

    Добавил библиотеку

--- ConsoleFileManager/Program.cs
+++ ConsoleFileManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileManager.Utils;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -28,20 +29,111 @@ namespace ConsoleFileManager
         }
 
         /// <summary>
+        /// Вспомогательный метод, получить текущую позицию курсора
+        /// </summary>
+        /// <returns></returns>
+        static (int left, int top) GetCursorPosition()
+        {
+            return (Console.CursorLeft, Console.CursorTop);
+        }
+
+        /// <summary>
         /// Обработка процесса ввода данных с консоли
         /// </summary>
         /// <param name="width">Длина строки ввода</param>
         static void ProcessEnterCommand(int width)
         {
             //TODO: ...
+            (int left, int top) = GetCursorPosition();
+            StringBuilder command = new StringBuilder();
+            ConsoleKeyInfo keyInfo;
+            char key;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+                key = keyInfo.KeyChar;
+
+                if (keyInfo.Key != ConsoleKey.Enter && keyInfo.Key != ConsoleKey.Backspace &&
+                    keyInfo.Key != ConsoleKey.UpArrow)
+                    command.Append(key);
+
+                (int currentLeft, int currentTop) = GetCursorPosition();
+
+                if (currentLeft == width - 2)
+                {
+                    Console.SetCursorPosition(currentLeft - 1, top);
+                    Console.Write(" ");
+                    Console.SetCursorPosition(currentLeft - 1, top);
+                }
+
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    if (command.Length > 0)
+                        command.Remove(command.Length - 1, 1);
+                    if (currentLeft >= left)
+                    {
+                        Console.SetCursorPosition(currentLeft, top);
+                        Console.Write(" ");
+                        Console.SetCursorPosition(currentLeft, top);
+                    }
+                    else
+                    {
+                        command.Clear();
+                        Console.SetCursorPosition(left, top);
+                    }
+                }
+
+            }
+            while (keyInfo.Key != ConsoleKey.Enter);
+            ParseCommandString(command.ToString());
         }
+
+        static void ParseCommandString(string command)
+        {
+            string[] commandParams = command.ToLower().Split(' ');
+            if (commandParams.Length > 0)
+            {
+                switch (commandParams[0])
+                {
+                    case "cd":
+                        if (commandParams.Length > 1)
+                            if (Directory.Exists(commandParams[1]))
+                            {
+                                currentDir = commandParams[1];
+                            }
+                        break;
+                    case "ls":
+                        if (commandParams.Length > 1 && Directory.Exists(commandParams[1]))
+                            if (commandParams.Length > 3 && commandParams[2] == "-p" && int.TryParse(commandParams[3], out int n))
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), n);
+                            }
+                            else
+                            {
+                                DrawTree(new DirectoryInfo(commandParams[1]), 1);
+                            }
+                        break;
+                }
+            }
+            UpdateConsole();
+        }
+
+        static string GetShortPath(string path)
+        {
+            StringBuilder shortPathName = new StringBuilder((int)API.MAX_PATH);
+            API.GetShortPathName(path, shortPathName, API.MAX_PATH);
+            return shortPathName.ToString();
+        }
+
+
 
         /// <summary>
         /// Обновление ввода с консоли
         /// </summary>
         static void UpdateConsole()
         {
-            DrawConsole(currentDir, 0, 26, WINDOW_WIDTH, 3);
+            DrawConsole(GetShortPath(currentDir), 0, 26, WINDOW_WIDTH, 3);
             ProcessEnterCommand(WINDOW_WIDTH);
         }
 
@@ -97,6 +189,75 @@ namespace ConsoleFileManager
             Console.Write("╝");
             Console.SetCursorPosition(x, y);
 
+        }
+
+        /// <summary>
+        /// Отрисовать дерево каталогов
+        /// </summary>
+        /// <param name="dir">Директория</param>
+        /// <param name="page">Страница</param>
+        static void DrawTree(DirectoryInfo dir, int page)
+        {
+            StringBuilder tree = new StringBuilder();
+            GetTree(tree, dir, "", true);
+            DrawWindow(0, 0, WINDOW_WIDTH, 18);
+            (int currentLeft, int currentTop) = GetCursorPosition();
+            int pageLines = 16;
+            string[] lines = tree.ToString().Split('\n');
+            int pageTotal = (lines.Length + pageLines - 1) / pageLines;
+            if (page > pageTotal)
+                page = pageTotal;
+
+            for (int i = (page - 1) * pageLines, counter = 0; i < page * pageLines; i++, counter++)
+            {
+                if (lines.Length - 1 > i)
+                {
+                    Console.SetCursorPosition(currentLeft + 1, currentTop + 1 + counter);
+                    Console.WriteLine(lines[i]);
+                }
+            }
+
+            // Отрисуем footer
+            string footer = $"╡ {page} of {pageTotal} ╞";
+            Console.SetCursorPosition(WINDOW_WIDTH / 2 - footer.Length / 2, 17);
+            Console.WriteLine(footer);
+
+        }
+
+        static void GetTree(StringBuilder tree, DirectoryInfo dir, string indent, bool lastDirectory)
+        {
+            tree.Append(indent);
+            if (lastDirectory)
+            {
+                tree.Append("└─");
+                indent += "  ";
+            }
+            else
+            {
+                tree.Append("├─");
+                indent += "│ ";
+            }
+
+            tree.Append($"{dir.Name}\n");
+
+
+            FileInfo[] subFiles = dir.GetFiles();
+            for (int i = 0; i < subFiles.Length; i++)
+            {
+                if (i == subFiles.Length - 1)
+                {
+                    tree.Append($"{indent}└─{subFiles[i].Name}\n");
+                }
+                else
+                {
+                    tree.Append($"{indent}├─{subFiles[i].Name}\n");
+                }
+            }
+
+
+            DirectoryInfo[] subDirects = dir.GetDirectories();
+            for (int i = 0; i < subDirects.Length; i++)
+                GetTree(tree, subDirects[i], indent, i == subDirects.Length - 1);
         }
     }
 }
